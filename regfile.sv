e509eaf6463fb5e986371e1071e056ed6e303273
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-14 13:51:48 +0000

    Change address width from 6 to 5 bits in regfile

--- regfile.sv
+++ regfile.sv
@@ -5,9 +5,9 @@ module regfile#(
     input logic clk,
     input logic [DATA_WIDTH-1:0] WD3,
     input logic WE3,
-    input logic [5:0] AD3,
-    input logic [5:0] AD2,
-    input logic [5:0] AD1,
+    input logic [4:0] AD3,
+    input logic [4:0] AD2,
+    input logic [4:0] AD1,
     output logic [DATA_WIDTH-1: 0] RD1,
     output logic [DATA_WIDTH-1: 0] RD2,
     output logic [DATA_WIDTH-1: 0] A0 
@@ -15,16 +15,15 @@ module regfile#(
 
     logic [DATA_WIDTH-1: 0] regfile_array [REG_FILE_WIDTH-1: 0];
 
-    always_ff (@posedge clk) begin
-        if(WE3) mem[AD3] = WD3;
+    always_ff @(posedge clk) begin
+        if(WE3) regfile_array[AD3] <= WD3;
     end
     
     always_comb begin
-        RD1= mem[AD1];
-        RD2= mem[AD2];
-        A0 = mem[10];
+        RD1= regfile_array[AD1];
+        RD2= regfile_array[AD2];
+        A0 = regfile_array[10];
     end
 
 endmodule
-
 
