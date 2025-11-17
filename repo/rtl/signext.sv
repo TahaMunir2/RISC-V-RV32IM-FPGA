388d23e8787cedd8a6017e00aad88cc90b2838d9
Cathugger8 <leoyin20@gmail.com>
2025-11-17 15:26:39 +0000

    cleaned up sign extension code

--- repo/rtl/signext.sv
+++ repo/rtl/signext.sv
@@ -7,22 +7,16 @@ module signext #(
     output logic [DATA_WIDTH-1 : 0] immext
 );
 
-wire [IMM_LENGTH-1 : 0] imm12;
-wire [IMM_LENGTH : 0] imm13;
-
 ///sign extension depending on value of ImmSrc
 always_comb begin
     if (ImmSrc == 2'b0) begin  
-        imm12 = instr[31:20];
-        immext = {{(DATA_WIDTH - IMM_LENGTH){imm12[IMM_LENGTH-1]}}, imm12};
+        immext = {{20{intr[31]}}, instr[31:20]};
     end
     else if (ImmSrc == 2'b1) begin 
-        imm12 = {instr[31:25], instr[11:7]};
-        immext = {{(DATA_WIDTH - IMM_LENGTH){imm12[IMM_LENGTH-1]}}, imm12};
+        immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
     end
     else if (ImmSrc == 2'b10) begin
-        imm13 = {instr[31], instr[7], instr[30:25], instr[11:8], 0'b0};
-        immext = {{(DATA_WIDTH - IMM_LENGTH + 1){imm13[IMM_LENGTH]}}, imm13};
+        immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
     end
 end
 endmodule
