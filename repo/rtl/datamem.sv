70ac46e7947da5138a41e039b29e0c1c4792ab5a
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-18 15:50:15 +0000

    Refactor datamem for asynchronous output and byte addressing
    Removed the clock input to design the data memory output as asynchronous. Updated the RAM array to 8-bit and modified output logic for byte addressing.

--- repo/rtl/datamem.sv
+++ repo/rtl/datamem.sv
@@ -1,21 +1,25 @@
 module datamem #(
     parameter ADDRESS_WIDTH = 32,
               DATA_WIDTH = 32
-)(
-    input  logic                  clk,
-    input  logic [ADDRESS_WIDTH-1:0] A,
+)(//removed the clock from the input because we are designing the datamem output to be asynchronous (when implementing store we might have to add the clock back in order to make the input to the data memory synchronous)
+    input  logic [ADDRESS_WIDTH-1:0] A, 
     output logic [DATA_WIDTH-1:0]    dout
 );
 
-logic [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH-1:0];
+logic [7:0] ram_array [2**ADDRESS_WIDTH-1:0];
 
 initial begin
     $display("Loading ram.");
     $readmemh("sineram.mem", ram_array);
 end
 
-always_ff @(posedge clk)
-    // output is synchronous
-    dout <= rom_array[A];
+// SYNCHRONOUS OR ASYNCHRONOUS? This is the Asynchronous way (so the whole instruction executes only in 1 clock cycle)
+always_comb begin
+    dout[7:0] = rom_array[A];
+    dout[8:15]= rom_array[A+1];
+    dout[16:23] = rom_array[A+2];
+    dout[24:31]= rom_array[A+3];
+end
+//byte addressing (and not word addressing)
 
 endmodule
