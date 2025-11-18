db4857319c3ebdff360ade05ace98c9165c97197
TahaMunir2 <137712196+TahaMunir2@users.noreply.github.com>
2025-11-18 01:06:00 +0000

    Update instruction memory parameters and ROM source
    Changed parameter DATA_WIDTH from 8 to 32 and updated addr input accordingly. Modified ROM loading to read from 'program.hex' instead of 'counterrom.mem'.

--- repo/rtl/insmem.sv
+++ repo/rtl/insmem.sv
@@ -1,15 +1,14 @@
 module insmem #(
-    parameter DATA_WIDTH = 8
+    parameter DATA_WIDTH = 32
 )(
-    input logic [31 : 0] addr,
+    input logic [DATA_WIDTH-1 : 0] addr,
     output logic [DATA_WIDTH-1 : 0] instr
 );
-logic [DATA_WIDTH-1 : 0] rom_array [255:0];
+logic [7 : 0] rom_array [255:0];
 
-///instr mem reads from a file called "counterrom.mem"
 initial begin
-    $display("Loading rom.");
-    $readmemh("counterrom.mem", rom_array);
+    $display("Loading rom.", );
+    $readmemh("../rtl/program.hex", rom_array);
 end;
 
 ///asynchronous instruction read
