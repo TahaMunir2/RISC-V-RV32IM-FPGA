d4624feb920c4dca11d976c66e035d44afc9084c
Ke1911 <yusuf.teker24@imperial.ac.uk>
2025-11-25 19:15:13 +0100

    Refactor hazard unit logic for better clarity

--- rtl/hazard_unit.sv
+++ rtl/hazard_unit.sv
@@ -9,40 +9,53 @@ module hazard_unit#(
     input logic [reg_addressing_width-1:0] rdWB,
     input logic regWriteM,
     input logic [1:0] resultSrCE,
+    input logic [1:0] resultSrCM,
     input logic WriteBack_Regfile,    
     output logic [1:0] selectline1,
     output logic [1:0] selectline2,
-    output logic insert_nop_exec,
-    output logic insert_nop_fd,
-    output logic F_D_Write,
+    output logic flush_d_exec,
+    output logic flush_f_d,
+    output logic F_Write,
     output logic PCWrite
 );
 
 logic wstall;
+//logic lw_write_back;
 
 assign wStall = (resultSrCE == 2'b01) &&
                  ( (rdE != 0) &&
-                   ( (rdE == rs1D) || (rdM == rs2D) ) );
+                   ( (rdE == rs1D) || (rdE == rs2D) ) );
 
+
+// assign lw_write_back = (resultSrCM == 2'b01) &&
+//                  ( (rdM != 0) &&
+//                    ( (rdM == rs1D) || (rdM == rs2D) ) );
 
 always_comb begin
 
     // Default: no stall, no flush
     PCWrite     = 1;
-    F_D_Write = 1;
-    insert_nop_exec = 0;
-    insert_nop_fd = 0;
+    F_Write = 1;
+    flush_d_exec = 0;
+    flush_f_d = 0;
 
     if(PCSrcE == 1) begin
-        insert_nop_fd = 1;
-        insert_nop_exec = 1;
+        flush_f_d = 1;
+        flush_d_exec = 1;
     end
 
     if(wStall == 1) begin
         PCWrite     = 0;
-        F_D_Write = 0;
-        insert_nop_exec = 1;
+        F_Write = 0;
+        flush_d_exec = 1;
     end
+
+    // if(lw_write_back == 1) begin
+    //     if((rdM == rs1D)) begin
+
+    //     end
+
+    //     ;
 
 
 
