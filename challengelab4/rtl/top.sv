22ace08650094dd038f5829f2bf7706eefef80a5
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-25 19:46:35 +0000

    Base layer for the pipeline top sheet
    Added additional logic for instruction decoding and data handling.

--- challengelab4/rtl/top.sv
+++ challengelab4/rtl/top.sv
@@ -16,13 +16,47 @@ module top #(
     logic PCSrc;
     logic [DATA_WIDTH-1:0] ImmOp;
     logic [DATA_WIDTH-1:0] PC;
-//extra logic: select line to the multiplexer selecting if we are writing from the datamem or from the ALU output
     logic reg_entry;
+    logic [DATA_WIDTH-1:0] instr_d;
 
+//extra logic added because we splitted the register file , alu and data memory block
+    logic [DATA_WIDTH-1: 0] output_ALU;
+    logic [DATA_WIDTH-1: 0] ALUop1;
+    logic [DATA_WIDTH-1: 0] ALUop2;
+    logic [DATA_WIDTH-1: 0] regOp2;
+    logic [DATA_WIDTH-1: 0] output_DataMem;
+    logic [DATA_WIDTH-1: 0] write_to_reg;
 
+/*
+module f-d_pipeline #(
+    DATA_WIDTH = 32
+) (
+    input logic clk,
+    input logic flush,
+    input logic enable,
+    input logic [DATA_WIDTH-1:0] instr_f,
+    input logic [DATA_WIDTH-1:0] pc_f,
+    input logic [DATA_WIDTH-1:0] pc_save_f,
+    output logic [DATA_WIDTH-1:0] instr_d,
+    output logic [DATA_WIDTH-1:0] pc_d,
+    output logic [DATA_WIDTH-1:0] pc_save_d
+);
+*/
+
+    fd_pipeline fd_pipeline(
+        .clk(clk),
+        .flush(),/////////SEE WHAT TO PUT HERE
+        input logic enable,/////////SEE WHAT TO PUT HERE
+        .instr_f(instr),
+        .pc_f(PC),
+        .pc_save_f(pc + 4),
+        .instr_d(instr_d),
+        .pc_d(),
+        output logic [DATA_WIDTH-1:0] pc_save_d  
+    );
     control control (
         .EQ(EQ),
-        .instr(instr),
+        .instr(instr_d),
         .RegWrite(RegWrite),
         .ALUCtrl(ALUCtrl),
         .ALUSrc(ALUSrc),
@@ -50,18 +84,51 @@ module top #(
         .pc(PC)
     );
 
-    regandalu Reg_and_ALU_block (
+
+    regfile regfile(
         .clk(clk),
-        .A0(a0),
-        .EQ(EQ),
-        .AD3(instr[11:7]),
-        .AD2(instr[24:20]),
-        .AD1(instr[19:15]),
-        .ImmOp(ImmOp),
-        .ALUsrc(ALUSrc),
-        .ALUctrl(ALUCtrl),
-        .reg_entry(reg_entry), //additional input (select line of the additional multiplexer) -> coming from the control block
-        .WE3(RegWrite)
+        .WD3(write_to_reg), //it is not anymore always the output of the ALU , it can be both (output of ALU and output of DataMem depending on the instruction)
+        .AD3(AD3),
+        .AD2(AD2),
+        .AD1(AD1),
+        .WE3(WE3),
+        .RD1(ALUop1),
+        .RD2(regOp2),
+        .A0(A0)
     );
 
+    mux mux(
+        .in0(regOp2),
+        .in1(ImmOp),
+        .sel(ALUsrc),
+        .out(ALUop2)
+    );
+
+    alu alu(
+        .ALUop1(ALUop1),
+        .ALUop2(ALUop2),
+        .ALUctrl(ALUctrl),
+        .ALUout(output_ALU),
+        .EQ(EQ)
+    );
+
+//additional blocks to the initial top module:
+
+//1) data memory (Asynchronous input) :
+
+    datamem datamem(
+        .A(output_ALU),
+        .dout(output_DataMem)
+    );
+
+//2) multiplexer to select where to write in the regfile from
+    mux mux_entry_regfile(
+        .in0(output_ALU),
+        .in1(output_DataMem),
+        .sel(reg_entry), //input taken from the control block
+        .out(write_to_reg)
+    );
+
+
 endmodule
+
