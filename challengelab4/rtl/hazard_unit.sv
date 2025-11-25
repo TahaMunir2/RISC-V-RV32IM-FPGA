4c1e29c2efcbacd945c60900eed7315ff161f1a0
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-25 19:20:44 +0000

    Implement forwarding logic in hazard_unit.sv
    Added forwarding logic for pipeline multiplexers in hazard unit.

--- challengelab4/rtl/hazard_unit.sv
+++ challengelab4/rtl/hazard_unit.sv
@@ -18,7 +18,36 @@ module hazard_unit#(
     output logic F_Write,
     output logic PCWrite
 );
+    
+//forwarding:
+//select line conventions for the pipeline multiplexers (in the execute stage):
+// no forwarding : select line = 00
+//forwarding from the memory stage (consecutive instructions Read-After-Write) : select line = 01
+//forwarding from the Write Back stage : select line = 10
 
+always_comb begin
+    //Default:no forwarding
+    selectline1 = 2'b00;
+    selectline2 = 2'b00;
+
+    //Operand1 forwarding
+    if (regWriteM && rdM != 0 && rdM == rs1) begin
+        selectline1 = 2'b10;  //from MEM stage
+    end 
+    else if (WriteBack_Regfile && rdWB !=0 && rdWB == rs1) begin
+        selectline1 = 2'b01;  //from WB stage
+    end
+
+    // Operand 2 forwarding
+    if (regWriteM && rdM !=0 && rdM == rs2) begin
+        selectline2 = 2'b10;
+    end 
+    else if (WriteBack_Regfile && rdWB !=0 && rdWB == rs2) begin
+        selectline2 = 2'b01;
+    end
+
+end
+    
 logic wstall;
 //logic lw_write_back;
 
