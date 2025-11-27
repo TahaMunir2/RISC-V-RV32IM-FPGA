2e6109a2460e342f398832b5d49f14b1cff061f6
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-27 15:59:48 +0000

    uploaded the branch predictor FSM

---
+++ challengelab4/rtl/branchpredictor_2bit.sv
@@ -0,0 +1,77 @@
+module branchpredictor_2bit #(
+    parameter TARGET_BUFFER_SIZE = 64, //number of branches we store, which is relatively small (in practice we don't have a big table for branch prediction because accessing a table would waste a lot of time and would make the design inneficient)
+    parameter INDEX_BITS = $clog2(TARGET_BUFFER_SIZE) //index size: how many bits we take from the PC to determine which specific branch we are refering to
+)(
+    input  logic clk,
+    input  logic rst,
+    input  logic [INDEX_BITS-1:0] predict_index,//index from PC (we take the bus [7:2] corresponding to 6 bits from PC to identify the specific jump we are dealing with)
+    input  logic enable,//enable signal for the FSM
+    input  logic [INDEX_BITS-1:0] update_index,  // index of that branch PC
+    input  logic actual_taken, //real outcome
+    output logic pred_taken//prediction output 
+);
+
+//state enumeration
+    typedef enum logic [1:0] {
+        STRONGLY_NOT_TAKEN = 2'b00,
+        WEAKLY_NOT_TAKEN   = 2'b01,
+        WEAKLY_TAKEN       = 2'b10,
+        STRONGLY_TAKEN     = 2'b11
+    } my_state;
+
+    my_state array [TARGET_BUFFER_SIZE-1:0]; //current state
+    my_state next_array [TARGET_BUFFER_SIZE-1:0]; //next state
+
+    //Advance state registers on clock or reset, FSM flip-flop
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            for (int i = 0; i <TARGET_BUFFER_SIZE; i++) //we initialize all the predictions to weakly not taken (no extreme initialization because we don't know about the branches yet)
+                array[i] <= WEAKLY_NOT_TAKEN;
+        end
+        else begin
+            for (int i = 0; i <TARGET_BUFFER_SIZE; i++)
+                array[i] <= next_array[i]; //we update the target buffer
+        end
+    end
+
+    always_comb begin
+        // default: we keep all current states
+        for (int i = 0; i <TARGET_BUFFER_SIZE;i++)
+            next_array[i] = array[i];
+
+        if (enable) begin
+            case (array[update_index])
+
+                STRONGLY_NOT_TAKEN: begin
+                    next_array[update_index] =actual_taken ? WEAKLY_NOT_TAKEN : STRONGLY_NOT_TAKEN;
+                end
+
+                WEAKLY_NOT_TAKEN: begin
+                    next_array[update_index] = actual_taken ? WEAKLY_TAKEN : STRONGLY_NOT_TAKEN;
+                end
+
+                WEAKLY_TAKEN: begin
+                    next_array[update_index] = actual_taken ? STRONGLY_TAKEN : WEAKLY_NOT_TAKEN;
+                end
+
+                STRONGLY_TAKEN: begin
+                    next_array[update_index] =actual_taken ? STRONGLY_TAKEN : WEAKLY_TAKEN;
+                end
+
+                default: begin
+                    next_array[update_index] = STRONGLY_NOT_TAKEN;
+                end
+
+            endcase
+        end
+    end
+
+    //Output logic(Moore: depends only on current state)
+    always_comb begin
+        pred_taken= array[predict_index][1]; //1 for 10 or 11 (describing the states: WEAKLY_TAKEN (10) and STRONGLY_TAKEN(11) )
+    end
+
+endmodule
+
+
+
