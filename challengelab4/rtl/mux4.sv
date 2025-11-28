c8d79c87a5363b8f677df40c52918ca9ce99812a
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-28 20:02:18 +0000

    Add files via upload

--- challengelab4/rtl/mux4.sv
+++ challengelab4/rtl/mux4.sv
@@ -1,29 +1,24 @@
 module mux4 #(
-    parameter DATA_WIDTH =32
+    parameter DATA_WIDTH = 32
 )(
-    input logic [DATA_WIDTH-1: 0] in0,
-    input logic [DATA_WIDTH-1: 0] in1,
-    input logic [DATA_WIDTH-1: 0] in2,
-    input logic [DATA_WIDTH-1: 0] in3,
-    input logic [1:0] select_line,
+    input  logic [DATA_WIDTH-1:0] in0,
+    input  logic [DATA_WIDTH-1:0] in1,
+    input  logic [DATA_WIDTH-1:0] in2,
+    input  logic [DATA_WIDTH-1:0] in3,
+    input  logic [1:0]            select_line,
     output logic [DATA_WIDTH-1:0] out
 );
 
-mux muxA (
-    .in0(in0),
-    .in1(in1),
-    .sel(select_line[0]),
-    .out(out)
-);
-
-mux muxB(
-    .in0(in2),
-    .in1(in3),
-    .sel(select_line[1]),
-    .out(out)
-);
-
-
+always_comb begin
+    case(select_line)
+        2'b00: out = in0;
+        2'b01: out = in1;
+        2'b10: out = in2;
+        2'b11: out = in3;
+        default: out = in0;
+    endcase
+end
 
 endmodule
+
 
