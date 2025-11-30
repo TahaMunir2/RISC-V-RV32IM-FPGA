d80b8c9c4118def603c809fde674dc1cffff6fcb
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-30 16:48:26 +0000

    Update verify.cpp

--- repo/tb/tests/verify.cpp
+++ repo/tb/tests/verify.cpp
@@ -1,49 +1,105 @@
-#include "testbench.h"
 #include <cstdlib>
+#include <utility>
+
+#include "cpu_testbench.h"
 
 #define CYCLES 10000
 
-unsigned int ticks = 0;
-
-class CpuTestbench : public Testbench
+TEST_F(CpuTestbench, TestAddiBne)
 {
-protected:
-    void initializeInputs() override
-    {
-        top->clk = 1;
-        top->rst = 0;
-    }
-};
-
-TEST_F(CpuTestbench, BaseProgramTest)
-{
-    bool success = false;
-    system("./compile.sh asm/program.S");
-
-    for (int i = 0; i < CYCLES; i++)
-    {
-        runSimulation(1);
-        if (top->a0 == 254)
-        {
-            SUCCEED();
-            success = true;
-            break;
-        }
-    }
-    if (!success)
-    {
-        FAIL() << "Counter did not reach 254" << top->a0;
-    }
+    setupTest("1_addi_bne");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 254);
 }
 
-// Note this is how we are going to test your CPU. Do not worry about this for
-// now, as it requires a lot more instructions to function
-// TEST_F(CpuTestbench, Return5Test)
-// {
-//     system("./compile.sh c/return_5.c");
-//     runSimulation(100);
-//     EXPECT_EQ(top->a0, 5);
-// }
+TEST_F(CpuTestbench, TestLiAdd)
+{
+    setupTest("2_li_add");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 1000);
+}
+
+TEST_F(CpuTestbench, TestLbuSb)
+{
+    setupTest("3_lbu_sb");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 300);
+}
+
+TEST_F(CpuTestbench, TestJalRet)
+{
+    setupTest("4_jal_ret");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 53);
+}
+
+TEST_F(CpuTestbench, TestPdf)
+{
+    setupTest("5_pdf");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES * 100);
+    EXPECT_EQ(top_->a0, 15363);
+}
+
+TEST_F(CpuTestbench, Testbeq)
+{
+    setupTest("6_beq");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 2);
+}
+
+TEST_F(CpuTestbench, TestSBandLW)
+{
+    setupTest("7_sb_lw");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 257);
+}
+
+TEST_F(CpuTestbench, TestBranchInequalities)
+{
+    setupTest("8_inequality_branching");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 2);
+}
+
+TEST_F(CpuTestbench, TestAUIPC)
+{
+    setupTest("9_auipc");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 4096);
+}
+
+TEST_F(CpuTestbench, TestShift)
+{
+    setupTest("10_unsigned_immediate_shift");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 2);
+}
+
+TEST_F(CpuTestbench, TestPdf2)
+{
+    setupTest("a0test");
+    setData("reference/gaussian.mem");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 155);
+}
+
 
 int main(int argc, char **argv)
 {
