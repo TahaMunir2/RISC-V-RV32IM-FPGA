bf7daf2ad850e423118261ec75ccd670ae9cf1fd
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-12-05 17:55:54 +0000

    Add files via upload

--- repo/tb/tests/alu_tb.cpp
+++ repo/tb/tests/alu_tb.cpp
@@ -1,74 +1,176 @@
-/*
- *  Verifies the results of the mux, exits with a 0 on success.
- */
+
+//Verifies ALU correctness for all supported operations.
+
 
 #include "base_testbench.h"
 
-Vdut *top;
-VerilatedVcdC *tfp;
 unsigned int ticks = 0;
 
-class ALUTestbench : public BaseTestbench
+class AluTestbench : public BaseTestbench
 {
 protected:
     void initializeInputs() override
     {
-        top->ALUctrl = 0;
         top->ALUop1 = 0;
         top->ALUop2 = 0;
+        top->ALUCtrl = 0;
+    }
+
+    inline void load(uint32_t a, uint32_t b, uint32_t aluCtrl)
+    {
+        top->ALUop1  = a;
+        top->ALUop2  = b;
+        top->ALUCtrl = aluCtrl;
+        top->eval();
     }
 };
 
-TEST_F(ALUTestbench, ALUTest0) // test addition
+//we will test the 13 different functionalities of the ALU:
+
+// 1) add
+TEST_F(AluTestbench, AddPositive)
 {
-    top->ALUctrl = 0;
-    top->ALUop1 = 5;
-    top->ALUop2 = 5;
-
-    top->eval();
-
-    EXPECT_EQ(top->ALUout, 10);
+    load(10,5, 0b0000); // 10+5 =15
+    EXPECT_EQ(top->ALUout, 15);
 }
 
-TEST_F(ALUTestbench, ALUTest1) // eq flag raised
+//special case where we add the biggest possible 32 bit integer and 1 to test the wrap around behaviour
+TEST_F(AluTestbench, AddWrap)
 {
-    top->ALUctrl = 1;
-    top->ALUop1 = 5;
-    top->ALUop2 = 5;
+    load(0xFFFFFFFF, 1, 0b0000);
+    EXPECT_EQ(top->ALUout, 0);
+}
 
-    top->eval();
+// 2)sub
+TEST_F(AluTestbench, SubPositive)
+{
+    load(10, 3, 0b0001);
+    EXPECT_EQ(top->ALUout, 7);
+}
+//special case where we get a negative result 
+TEST_F(AluTestbench, SubNegative)
+{
+    load(3, 10, 0b0001);
+    EXPECT_EQ(top->ALUout, uint32_t(-7));
+}
 
+//BITWISE OPERATIONS:
+
+//3)and
+TEST_F(AluTestbench,AndOp)
+{
+    load(0b1010, 0b0110, 0b0010);
+    EXPECT_EQ(top->ALUout, 0b0010);
+}
+
+//4-or
+TEST_F(AluTestbench,OrOp)
+{
+    load(0b1010, 0b0110, 0b0011);
+    EXPECT_EQ(top->ALUout, 0b1110);
+}
+
+//5)xor
+TEST_F(AluTestbench, XorOp)
+{
+    load(0b1010, 0b0110, 0b0100);
+    EXPECT_EQ(top->ALUout, 0b1100);
+}
+
+
+//shift operations:
+//6)sll
+TEST_F(AluTestbench, ShiftLeftLogical)
+{
+    load(0x00000001, 3, 0b0101);
+    EXPECT_EQ(top->ALUout, 0x00000008);
+}
+
+
+//7-srl
+
+TEST_F(AluTestbench, ShiftRight_Logical)
+{
+    load(0x10, 2, 0b0110);
+    EXPECT_EQ(top->ALUout, 0x4);
+}
+
+//8-sra
+TEST_F(AluTestbench, ShiftRight_Arithmetic)
+{
+    load(0xFFFFFFFF, 1, 0b0111);
+    EXPECT_EQ(top->ALUout, 0xFFFFFFFF);
+}
+
+//9)SLT (signed)
+TEST_F(AluTestbench, SetLessThan_Signed)
+{
+    load(-5, 10, 0b1000);
+    EXPECT_EQ(top->ALUout, 1);
+}
+//opposite case
+TEST_F(AluTestbench, SetLessThan_SignedFalse)
+{
+    load(20, -1, 0b1000);
+    EXPECT_EQ(top->ALUout, 0);
+}
+
+//10) SLTU(unsigned)
+TEST_F(AluTestbench, SetLessThan_Unsigned)
+{
+    load(5, 10, 0b1001);
+    EXPECT_EQ(top->ALUout, 1);
+}
+
+TEST_F(AluTestbench, SetLessThan_UnsignedFalse)
+{
+    load(10, 5, 0b1001);
+    EXPECT_EQ(top->ALUout, 0);
+}
+
+//11- LUI passthrough (no actions taken on the data)
+TEST_F(AluTestbench, LuiImmediate)
+{
+    load(0,0xABCDE000, 0b1010);
+    EXPECT_EQ(top->ALUout, 0xABCDE000); //same exact data as the one passed in input as b 
+}
+
+
+//12)AUIPC:A +B- 4
+TEST_F(AluTestbench, Auipc)
+{
+    load(100, 20, 0b1011);
+    EXPECT_EQ(top->ALUout, 100 + 20 - 4);
+}
+
+//13)Equality using flags
+TEST_F(AluTestbench, EqualTrue)
+{
+    load(10,10,0);
     EXPECT_EQ(top->EQ, 1);
 }
 
-TEST_F(ALUTestbench, ALUTest2) // eq flag reset
+TEST_F(AluTestbench, EqualFalse)
 {
-    top->ALUctrl = 1;
-    top->ALUop1 = 5;
-    top->ALUop2 = 4;
-
-    top->eval();
-
+    load(1,2,0);
     EXPECT_EQ(top->EQ, 0);
 }
 
+TEST_F(AluTestbench, SignedLessTrue)
+{
+    load(-5, 3, 0);
+    EXPECT_EQ(top->LT, 1);
+}
+
+TEST_F(AluTestbench, UnsignedLessTrue)
+{
+    load(3,10,0);
+    EXPECT_EQ(top->LTU, 1);
+}
+
+
 int main(int argc, char **argv)
 {
-    top = new Vdut;
-    tfp = new VerilatedVcdC;
-
-    Verilated::traceEverOn(true);
-    top->trace(tfp, 99);
-    tfp->open("waveform.vcd");
-
     testing::InitGoogleTest(&argc, argv);
-    auto res = RUN_ALL_TESTS();
-
-    top->final();
-    tfp->close();
-
-    delete top;
-    delete tfp;
-
-    return res;
+    return RUN_ALL_TESTS();
 }
--- repo/tb/tests/verify.cpp
+++ repo/tb/tests/verify.cpp
@@ -1,49 +1,89 @@
-#include "testbench.h"
 #include <cstdlib>
+#include <utility>
+
+#include "cpu_testbench.h"
 
 #define CYCLES 10000
 
-unsigned int ticks = 0;
-
-class CpuTestbench : public Testbench
+TEST_F(CpuTestbench, TestLiAdd)
 {
-protected:
-    void initializeInputs() override
-    {
-        top->clk = 1;
-        top->rst = 0;
-    }
-};
-
-TEST_F(CpuTestbench, BaseProgramTest)
-{
-    bool success = false;
-    system("./compile.sh asm/program.S");
-
-    for (int i = 0; i < CYCLES; i++)
-    {
-        runSimulation(1);
-        if (top->a0 == 254)
-        {
-            SUCCEED();
-            success = true;
-            break;
-        }
-    }
-    if (!success)
-    {
-        FAIL() << "Counter did not reach 254" << top->a0;
-    }
+    setupTest("2_li_add");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 1000);
 }
 
-// Note this is how we are going to test your CPU. Do not worry about this for
-// now, as it requires a lot more instructions to function
-// TEST_F(CpuTestbench, Return5Test)
-// {
-//     system("./compile.sh c/return_5.c");
-//     runSimulation(100);
-//     EXPECT_EQ(top->a0, 5);
-// }
+TEST_F(CpuTestbench, TestSubAdd)
+{
+    setupTest("sup_sub_add");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 4);
+}
+
+TEST_F(CpuTestbench, TestRAWChain)
+{
+    setupTest("straight_raw_chain");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 24);
+}
+
+TEST_F(CpuTestbench, SimpleWAW)
+{
+    setupTest("waw_simple");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 99);
+}
+
+TEST_F(CpuTestbench, warsimple)
+{
+    setupTest("war_simple");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 5);
+}
+
+TEST_F(CpuTestbench,parrallelism)
+{
+    setupTest("parrallelism");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 40);
+}
+
+TEST_F(CpuTestbench, ReUsingRegisters)
+{
+    setupTest("reg_reuse");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 100);
+}
+
+TEST_F(CpuTestbench, logicalops)
+{
+    setupTest("logical");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 100);
+}
+
+TEST_F(CpuTestbench, shifts)
+{
+    setupTest("sup_shifts");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 128);
+}
+
+TEST_F(CpuTestbench, complexshifts)
+{
+    setupTest("complexshifts");
+    initSimulation();
+    runSimulation(CYCLES);
+    EXPECT_EQ(top_->a0, 44);
+}
 
 int main(int argc, char **argv)
 {
