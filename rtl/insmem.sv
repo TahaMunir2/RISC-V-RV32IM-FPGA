dd53fea39b6ec8eb45c5bc240c7c752676813b72
TahaMunir2 <137712196+TahaMunir2@users.noreply.github.com>
2025-12-06 17:51:33 +0000

    Update insmem.sv

--- rtl/insmem.sv
+++ rtl/insmem.sv
@@ -1,19 +1,27 @@
 module insmem #(
-    parameter DATA_WIDTH = 32
+    parameter ADDRESS_WIDTH = 32,
+    parameter DATA_WIDTH    = 8,
+    parameter BASE_ADDR     = 32'hBFC00000
 )(
-    input logic [DATA_WIDTH-1 : 0] addr,
-    output logic [DATA_WIDTH-1 : 0] instr
+    input logic [ADDRESS_WIDTH-1:0] addr,
+    output logic [31:0] instr
 );
-logic [7 : 0] rom_array [2**17-1:0];
 
-initial begin
-    $display("Loading rom.", );
-    $readmemh("program.hex", rom_array);
-end;
+    logic [DATA_WIDTH-1:0] romArray [0:4095];
 
-///asynchronous instruction read
-always_comb begin
-    instr = {rom_array[addr + 3], rom_array[addr + 2], rom_array[addr + 1], rom_array[addr]};
-end;
+    initial begin
+        $display("Loading Instruction Memory");
+        $readmemh("program.hex", romArray);
+    end
+
+    logic [ADDRESS_WIDTH-1:0] local_addr;
+    assign local_addr = addr - BASE_ADDR;
+
+
+    always_comb begin
+        if (addr >= BASE_ADDR && local_addr < 4096) begin // check to make sure we aren't accessing memory we aren't allowed to
+            instr = {romArray[local_addr+3], romArray[local_addr+2], romArray[local_addr+1], romArray[local_addr]};
+        end else instr = 32'h00000000; // no-op for invalid pc
+    end
 
 endmodule
