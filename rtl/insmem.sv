8f1694144bb9d5255097f3d9369a426d1a1e0322
TahaMunir2 <137712196+TahaMunir2@users.noreply.github.com>
2025-12-10 15:50:26 +0000

    Update insmem.sv

--- rtl/insmem.sv
+++ rtl/insmem.sv
@@ -1,19 +1,30 @@
 module insmem #(
-    parameter DATA_WIDTH = 32
+    parameter DATA_WIDTH = 32,
 )(
-    input logic [DATA_WIDTH-1 : 0] addr,
-    output logic [DATA_WIDTH-1 : 0] instr
+    input  logic [DATA_WIDTH-1:0] addr,
+    output logic [DATA_WIDTH-1:0] instr
 );
-logic [7 : 0] rom_array [2**17-1:0];
 
-initial begin
-    $display("Loading rom.", );
-    $readmemh("program.hex", rom_array);
-end;
+    logic [7:0] rom_array [2**12-1:0];
 
-///asynchronous instruction read
-always_comb begin
-    instr = {rom_array[addr + 3], rom_array[addr + 2], rom_array[addr + 1], rom_array[addr]};
-end;
+    initial begin
+        $display("Loading rom.", );
+        $readmemh("program.hex", rom_array, 32'hBFC00000);
+    end
 
+    logic [ADDR_WIDTH-1 : 0] local_addr;
+
+    always_comb begin // asynchronous read
+        local_addr = addr - BASE_ADDR;
+        if (addr >= BASE_ADDR && local_addr < (MEM_DEPTH - 3)) begin
+            instr = {
+                rom_array[local_addr + 3], 
+                rom_array[local_addr + 2], 
+                rom_array[local_addr + 1], 
+                rom_array[local_addr]
+            };
+        end else begin
+            instr = 32'b0; 
+        end
+    end
 endmodule
