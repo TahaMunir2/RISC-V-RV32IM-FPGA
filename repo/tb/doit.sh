3b71842704954e4e82dfe55231fc03951cb105ef
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-12-10 13:57:24 +0000

    Update doit.sh

--- repo/tb/doit.sh
+++ repo/tb/doit.sh
@@ -74,12 +74,5 @@ for file in "${files[@]}"; do
     
 done
 
-# Exit as a pass or fail (for CI purposes)
-if [ $fails -eq 0 ]; then
-    echo "${GREEN}Success! All ${passes} test(s) passed!"
-    exit 0
-else
-    total=$((passes + fails))
-    echo "${RED}Failure! Only ${passes} test(s) passed out of ${total}."
-    exit 1
-fi
+# Save obj_dir in test_out
+mv obj_dir test_out/
