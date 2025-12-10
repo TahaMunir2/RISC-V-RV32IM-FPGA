ad195faed9d535913926f209758dc09074a43d30
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-12-10 13:42:48 +0000

    Update 10_unsigned_immediate_shift.s

--- repo/tb/asm/10_unsigned_immediate_shift.s
+++ repo/tb/asm/10_unsigned_immediate_shift.s
@@ -1,6 +1,11 @@
 .text
 .globl main
 main:
-    li t1, 1
-    slli a0, t1, 2
-    srli a0, a0, 1
+    li t1, 5          
+    li t2, 3         
+    slli t3, t1, 2    
+    add t4, t3, t2    
+    srli t5, t4, 1    
+    xor a0, t5, t2    
+
+    
